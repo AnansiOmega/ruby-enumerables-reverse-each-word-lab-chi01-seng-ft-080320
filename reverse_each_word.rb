c81e77342a9a75ca6561a66b50ef99391a27fcef
Ignas Butautas <ignasbutautas7@gmail.com>
2020-07-24 17:58:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'pry'
 def reverse_each_word(string)
   string = string.split(" ")
@@ -6,4 +7,14 @@ def reverse_each_word(string)
   r.reverse
 end
 final_string.join(" ")
+=======
+def reverse_each_word(string)
+  string = string.split(" ")
+  
+  string.collect do |r|
+  r.reverse
+ x = [] <<  r.reverse
+ x.join(" ")
+end
+>>>>>>> 8078b4e66e7da4a0b60ae825cb5079945ff6d07e
 end
